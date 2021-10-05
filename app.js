1c49b180aa11f80b96e974d61ee2fe6b355b73f9
ilham tenriajeng <ilhamtenriajeng03@gmail.com>
2021-10-06 05:16:37 +0800

    update app.js

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.use(router);
 
-const PORT = process.env.SERVER_PORT || 8000;
+const PORT = process.env.PORT || 8000;
 
 app.listen(PORT, () => {
 	console.log(`Server is running on http://localhost:${PORT}`);
